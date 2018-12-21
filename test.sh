2e9d3ab6458ac4f2341b7c76d26c7fa16d069046
D Malone <dmf@Ds-MacBook-Air.local>
2018-12-21 11:50:28 -0800

    feat: implement --test arguments for python commands

--- test.sh
+++ test.sh
@@ -15,13 +15,14 @@ EOF
 
 function test {
 	echo -e "*** SETTING UP TEST DATABASE ***\n" 
-	python test_database_setup.py 
+	python database_setup.py --test 
+	# (RIGHT NOW REAL DATABASE POPULATION CAN'T FUNCTION WHILE A TEST DATABASE IS CREATED)
 	echo -e "*** POPULATING DATABASE ***\n"
-	python test_database_populator.py
+	python database_populator.py --test
 	echo -e "*** TESTING PROGRAMS ***\n"
 	python test_app.py
 	echo -e "*** DELETING DATABASE ***\n"
-	rm test_database.db
+	rm test.db
 	echo -e "\nTesting completed."
 }
 
